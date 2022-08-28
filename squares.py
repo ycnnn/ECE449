d0dcd43742d535aab58a0da63ad2b3c7058a6ad5
ycnnn <62529545+ycnnn@users.noreply.github.com>
2022-08-28 16:27:59 -0500

    Create squares.py

---
+++ squares.py
@@ -0,0 +1,14 @@
+import numpy 
+import torch
+import matplotlib.pyplot as plt
+
+## You only need to complete the two functions.
+def numpy_squares(k):
+    x = numpy.power(numpy.arange(1,k + 1,1),2)
+    return x
+
+def torch_squares(k):
+    x = torch.square(torch.arange(1, k+1))
+    return x
+
+x = torch_squares(10)
